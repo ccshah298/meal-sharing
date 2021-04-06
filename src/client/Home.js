4f01262ebb01d1221ec8b198e6fca97ce519ff13
ccshah298 <ccshah298@gmail.com>
2021-04-06 12:47:17 +0200

    pic added

--- src/client/Home.js
+++ src/client/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+var foodmenu3 = path.basename("/src/client/assets/images/foodmenu3.png");
 
 export default function Home() {
   return (
       <div className="mealName">
-          <img src="/src/client/assets/images/foodmenu3.png" alt="foodmenu" height="600" width="600"></img>
+          <img src={foodmenu3} alt="foodmenu" height="600" width="600"></img>
           <p>Welcome to MyMeal App</p>
           <p>Created by Charmi</p>
       </div>
