47266a9ccfa476f34462dd14755b1947b2c1a88a
ccshah298 <ccshah298@gmail.com>
2021-04-06 13:06:32 +0200

    test pic

--- src/client/Home.js
+++ src/client/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
-const path = require('path');
-var foodmenu3 = path.basename("/src/client/assets/images/foodmenu3.png");
+//const path = require('path');
+//var foodmenu3 = path.basename("/src/client/assets/images/foodmenu3.png");
 
 export default function Home() {
   return (
       <div className="mealName">
-          <img src={foodmenu3} alt="foodmenu" height="600" width="600"></img>
+          <img src="https://i.ibb.co/LPXSj0S/foodmenu3.png" alt="foodmenu" height="600" width="600"></img>
           <p>Welcome to MyMeal App</p>
           <p>Created by Charmi</p>
       </div>
